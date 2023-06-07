f7eefe1973efbfcb36a1bacbccd221df383195fb
Kim Leakhena <kimleakhena08@gmail.com>
2023-06-07 16:00:27 +0700

    myfile

--- src/main/java/com/midterm/demo/controllers/OrderController.java
+++ src/main/java/com/midterm/demo/controllers/OrderController.java
@@ -3,7 +3,10 @@ package com.midterm.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -26,6 +29,13 @@ public class OrderController {
         });
         
         return "redirect:/giccafe/order";
+    }
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping(path = "/gicadmin/history")
+    public String order(Model model){
+        model.addAttribute("orders", service.getLatestOrder());
+        return "history";
+        
     }
 
 }
--- src/main/java/com/midterm/demo/service/OrderService.java
+++ src/main/java/com/midterm/demo/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.midterm.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,9 @@ public class OrderService {
 
     public Order getLastestOrder(){
         return repository.getLastestOrder();
+    }
+    public List<Order> getLatestOrder(){
+        return repository.findAll();
     }
 
 }
